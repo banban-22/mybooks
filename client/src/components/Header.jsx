f2f60ac8cc745bcd07b1d709c3e601da7923ce78
a1ya1k2a9@gmail.com <a1ya1k2a9@gmail.com>
2023-12-16 12:26:23 -0800

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 // import Link from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { CURRENT_USER } from '../queries/CurrentUser';
 
-const Header = () => {
+const Header = ({}) => {
+  const { data } = useQuery(CURRENT_USER);
+
   return (
-    <div className="w-full h-full flex justify-between items-center mx-auto bg-primary-light">
+    <div className="w-full h-full flex justify-between items-center mx-auto py-5 bg-primary-light">
       <p className="px-10 text-4xl font-bold tracking-widest">MyBooks</p>
       <div>
         <ul className="px-10 flex gap-5">
-          <li>Books</li>
-          <li>Name</li>
+          <li>{data.user.name}</li>
         </ul>
       </div>
     </div>
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { CURRENT_USER } from '../queries/CurrentUser';
+
 const Books = () => {
+  const navigate = useNavigate();
+  const { loading, error, data, refetch } = useQuery(CURRENT_USER);
+  console.log(data);
+
+  useEffect(() => {
+    if (data.user) navigate('/books');
+  }, [data.user, navigate]);
   return <div>Books</div>;
 };
 export default Books;
